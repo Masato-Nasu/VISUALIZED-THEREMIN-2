2960b4ae3ebfd5fcb71928b87dac053968de652c
那須　雅人 <1bitexist@gmail.com>
2025-12-13 13:37:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-// sw.js v3.4.7
-const CACHE='baby-theremin-pd-v3-4-7';
+// sw.js v3.4.8
+const CACHE='baby-theremin-pd-v3-4-8';
 const ASSETS=[
   './',
-  './index.html?v=3.4.7',
-  './manifest.json?v=3.4.7',
+  './index.html?v=3.4.8',
+  './manifest.json?v=3.4.8',
   './icon-192.png',
   './icon-512.png',
   './Chime.mp3'
@@ -12,6 +12,6 @@ self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.add
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
 self.addEventListener('fetch',e=>{
   const req=e.request;
-  if(req.mode==='navigate'){ e.respondWith(fetch(req).catch(()=>caches.match('./index.html?v=3.4.7'))); return; }
+  if(req.mode==='navigate'){ e.respondWith(fetch(req).catch(()=>caches.match('./index.html?v=3.4.8'))); return; }
   e.respondWith(caches.match(req).then(c=>c||fetch(req).then(r=>{ const copy=r.clone(); caches.open(CACHE).then(cc=>cc.put(req,copy)); return r; })));
 });
